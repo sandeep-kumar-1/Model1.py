5ad2f58b79513e8c8150f180d0973eec2860780e
sandeep-kumar-1 <49430334+sandeep-kumar-1@users.noreply.github.com>
2021-01-28 15:27:14 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import cv2
+emotion_data = pd.read_csv('/content/drive/My Drive/Emotion_Detection /fer2013.csv')
+print('emotion_data')
+X_train = []
+y_train = []
+X_test = []
+y_test = []
+for index, row in emotion_data.iterrows():
+    k = row['pixels'].split(" ")
+    if row['Usage'] == 'Training':
+        X_train.append(np.array(k)
+        y_train.append(row['emotion'])
+    elif row['Usage'] == 'PublicTest':
+        X_test.append(np.array(k))
+        y_test.append(row['emotion'])
+X_train = np.array(X_train')
+y_train = np.array(y_train)
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
+X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
+
+y_train= np_utils.to_categorical(y_train, num_classes=7)
+y_test = np_utils.to_categorical(y_test, num_classes=7)
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=(48,48,1)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+//compile:model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
+
